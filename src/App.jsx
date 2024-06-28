35ab39e4bfc3e4939c961e4dfd9ddbbd6c338abb
ayelabilal <ayelabilal944@gmail.com>
2024-06-28 20:23:36 +0500

    complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const Head = ()=>{
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <p>today is our first class...</p>
+      <input type="text" />
+      <img src="./vite.svg" alt="" />
+
+    </div>
+  )
+}
+
+export default Head;
